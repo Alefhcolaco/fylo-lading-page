8c6aeeb278aadce9191fbd2b69a9973060bbe112
gabriel <gleonardoalano@gmail.com>
2023-03-15 17:45:32 -0300

    fix: button component

--- components/Button1/index.js
+++
@@ -1,5 +0,0 @@
-export const Button1 = () => {
-    return(
-        <button className='bg-[#4CC1D2] text-[#FFFFFF] rounded-[30px] w-[306px] h-[62px] block m-auto font-semibold	hover:bg-[#89DDE2]'>Get Started</button>
-    );
-}
--- components/Button2/index.js
+++
@@ -1,5 +0,0 @@
-export const Button2 = () => {
-    return(
-        <button className='bg-[#4CC1D2] text-[#FFFFFF] rounded-[30px] w-[255px] h-[60px] font-semibold hover:bg-[#89DDE2]'>Get Started For Free</button>
-    );
-}
